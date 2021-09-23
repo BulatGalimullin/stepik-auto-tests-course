fa8ae9087fee15d548c931b05a9604aa5645a207
bigalimullin <bulat-003@yandex.ru>
2021-09-23 12:47:38 +0300

    Обновление тестов

---
+++ Tasks/lesson3_3_step3_test_in_pytest_style_test.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+import pytest
+
+def test_registration1():
+    try:
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector("input[class = 'form-control first'][required]")
+        input1.send_keys('hello')
+        input2 = browser.find_element_by_css_selector("input[class = 'form-control second'][required]")
+        input2.send_keys('h3i')
+        input3 = browser.find_element_by_css_selector("input[class = 'form-control third'][required]")
+        input3.send_keys('hi')
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert welcome_text == "Congratulations! You have successfully registered!", "Text messages do not match each other"
+    finally:
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+def test_registration2():
+    try:
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector("input[class = 'form-control first'][required]")
+        input1.send_keys('hello')
+        input2 = browser.find_element_by_css_selector("input[class = 'form-control second'][required]")
+        input2.send_keys('h3i')
+        input3 = browser.find_element_by_css_selector("input[class = 'form-control third'][required]")
+        input3.send_keys('hi')
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert welcome_text == "Congratulations! You have successfully registered!", "Text messages do not match each other"
+    finally:
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+
